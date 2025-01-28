e89f893b7a0d9866f2858ca8e5927052fc5fc729
bsbert <bsbertmuniz@gmail.com>
2025-01-28 11:39:28 +0100

    vuetify

--- calendario-youtube/src/main.js
+++ calendario-youtube/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 createApp(App).use(store).use(router).mount('#app')
+
+new Vue({
+    vuetify,
+    render: h => h(App)
+}).$mount("#app");
